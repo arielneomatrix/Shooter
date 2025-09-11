90b34e7b92a19a0e5efb9637a417f405760b46ee
Diegolo2 <albad2346@gmail.com>
2025-09-10 23:44:16 -0400

    Creando balas

--- Assets/Scripts/Shotter.cs
+++ Assets/Scripts/Shotter.cs
@@ -1,11 +1,14 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shotter : MonoBehaviour
 {
+   public Balas balasPrefab;
    public Rigidbody rigidbody;
-   public float speed = 7;
+ public Transform spawnPoint;
+    public float speed = 7;
 
     void Update()
     {
@@ -13,6 +16,7 @@ public class Shotter : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
+            
             Vector3 hitPoint = hit.point;
             hitPoint.y = transform.position.y;
             transform.forward = hitPoint - transform.position;
@@ -21,11 +25,18 @@ public class Shotter : MonoBehaviour
         transform.position += GetImput() * speed * Time.deltaTime;
         //rigidbody.velocity = GetImput() * speed;
 
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+            Balas balas = Instantiate(balasPrefab);
+            balas.transform.position = spawnPoint.position;
+            balas.transform.forward = transform.forward;
 
-    }
+        }
 
     Vector3 GetImput()
     {
         return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
     }
+}
 }
