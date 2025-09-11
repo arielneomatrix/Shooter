eec9cced6305031f2792f5db419feaf3aa2e2888
Diegolo2 <albad2346@gmail.com>
2025-09-11 16:43:48 -0400

    Enemigos te persiguen

--- Assets/Scripts/Balas.cs
+++ Assets/Scripts/Balas.cs
@@ -7,12 +7,12 @@ public class Balas : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * 20;
+        transform.position += transform.forward * Time.deltaTime * 100;
     }
 }
---
+++ Assets/Scripts/ZombieEnemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieEnemy : MonoBehaviour
+{
+    private Shotter _player;
+    
+    private void Start()
+    {
+        _player = FindObjectOfType<Shotter>();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ensure _player.transform is cast to Transform to access the position property
+        if (_player != null && _player.transform is Transform playerTransform)
+        {
+            transform.forward = playerTransform.position - transform.position;
+            transform.position += transform.forward * 1 * Time.deltaTime;
+        }
+    }
+}
